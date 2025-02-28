9167011484427b747c8d30e5be2511d914a40ea1
Ibrahima <ibou9543@gmail.com>
2025-02-28 20:51:08 +0000

    chnage keyboard reading events

--- Explorer/navigations.py
+++ Explorer/navigations.py
@@ -38,35 +38,38 @@ def naviguer(path):
                     path = parent_path
                     selected_index = 0
                     files = list(Path(path).iterdir())  # Mise à jour après retour
-            elif keyboard.is_pressed('shift+d') and files:
-                message = delete(files[selected_index])
-                files = list(Path(path).iterdir())  # Mettre à jour après suppression
-            elif keyboard.is_pressed('shift+r') and files:
-                time.sleep(0.1)
-                message = rename(files[selected_index])
-                files = list(Path(path).iterdir())  # Mettre à jour après renommage
-            elif keyboard.is_pressed("shift+c") and files:
-                copy = str(files[selected_index])
-                message = 'Copié'
-            elif keyboard.is_pressed("shift+v"):
-                if copy:
-                    message = paste(copy, str(path), action)
-                    copy = ''
-                    if action == 'move':
-                        action = 'paste'
-                    files = list(Path(path).iterdir())  # Mise à jour après collage
-                else:
-                    message = 'Rien à coller'
-            elif keyboard.is_pressed("shift+x") and files:
-                copy = str(files[selected_index])
-                message = 'Coupé'
-                action = 'move'
-            elif keyboard.is_pressed('shift+k'):
-                files, message = sort_by_type(path)
-            elif keyboard.is_pressed('shift+s'):
-                files, message = sort_by_size(path)
-            elif keyboard.is_pressed('shift+t'):
-                files, message = sort_by_date(path)
+            elif key == 'shift' and files:
+                time.sleep(0.2)
+                combi = keyboard.read_event().name
+                if combi == "d":
+                    message = delete(files[selected_index])
+                    files = list(Path(path).iterdir())  # Mettre à jour après suppression
+                elif combi == 'r':
+                    time.sleep(0.1)
+                    message = rename(files[selected_index])
+                    files = list(Path(path).iterdir())  # Mettre à jour après renommage
+                elif combi == 'c':
+                    copy = str(files[selected_index])
+                    message = 'Copié'
+                elif combi == 'v':
+                    if copy:
+                        message = paste(copy, str(path), action)
+                        copy = ''
+                        if action == 'move':
+                            action = 'paste'
+                        files = list(Path(path).iterdir())  # Mise à jour après collage
+                    else:
+                        message = 'Rien à coller'
+                elif combi == 'x':
+                    copy = str(files[selected_index])
+                    message = 'Coupé'
+                    action = 'move'
+                elif combi == 'k':
+                    files, message = sort_by_type(path)
+                elif combi == 's':
+                    files, message = sort_by_size(path)
+                elif combi == 't':
+                    files, message = sort_by_date(path)
 
         except Exception as e:
             message = f"Erreur : {e}"
